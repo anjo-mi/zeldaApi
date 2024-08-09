95a59556a13392fe7c5997c0126141a142a528cc
kyle <nojaim.k@gmail.com>
2024-08-08 23:36:58 -0400

    all conditionals placed for search, will need to mess w hidden classes

--- js/main.js
+++ js/main.js
@@ -2,7 +2,17 @@ document.getElementById('entry').addEventListener('keypress', getEntry)
 
 
 
-// let urlWholeCompendium = 'https://botw-compendium.herokuapp.com/api/v3/compendium/all'
+let urlWholeCompendium = 'https://botw-compendium.herokuapp.com/api/v3/compendium/all'
+
+const getAllEntries = url =>{
+    fetch(url)
+        .then(res => res.json())
+        .then(data => 
+            console.log(data)
+        )
+    }
+
+getAllEntries(urlWholeCompendium)
 
 // data.data.sort((a,b) => a.id - b.id)
 
@@ -20,8 +30,50 @@ function getEntry(event){
                     document.querySelector('.monster>.basicInfo>.img').src = data.data.image
                     document.querySelector('.monster>.basicInfo>.name').textContent = data.data.name
                     document.querySelector('.monster>.location').textContent = data.data.common_locations.join(', ')
-                    document.querySelector('.monster>.drops').textContent = data.data.drops
+                    document.querySelector('.monster>.drops').textContent = data.data.drops.join(', ')
                     document.querySelector('.monster>.details').textContent = data.data.description
+                }
+                if (data.data.category === 'equipment'){
+                    document.querySelector('.equip>.basicInfo>.category').textContent = data.data.category
+                    document.querySelector('.equip>.basicInfo>.img').src = data.data.image
+                    document.querySelector('.equip>.basicInfo>.name').textContent = data.data.name
+                    document.querySelector('.equip>.location').textContent = data.data.common_locations.join(', ')
+                    document.querySelector('.equip>.details').textContent = data.data.description
+                    if (data.data.properties.attack){    
+                        document.querySelector('.equip>.attOrDef').textContent = `Attack: ${data.data.properties.attack}`
+                    }else{
+                        document.querySelector('.equip>.attOrDef').textContent = `Defense: ${data.data.properties.defense}`
+
+                    }
+                    // document.querySelector('.equip>.recovery').textContent = `${data.data.hearts_recovered} Hearts Recovered (Raw)`
+                }
+                if (data.data.category === 'materials'){
+                    document.querySelector('.material>.basicInfo>.category').textContent = data.data.category
+                    document.querySelector('.material>.basicInfo>.img').src = data.data.image
+                    document.querySelector('.material>.basicInfo>.name').textContent = data.data.name
+                    document.querySelector('.material>.location').textContent = data.data.common_locations.join(', ')
+                    document.querySelector('.material>.details').textContent = data.data.description
+                    document.querySelector('.material>.cooked').textContent = `Cooking Effects: ${data.data.cooking_effect}`
+                    document.querySelector('.material>.recovery').textContent = `${data.data.hearts_recovered} Hearts Recovered (Raw)`
+                }
+                if (data.data.category === 'creatures'){
+                    document.querySelector('.creature>.basicInfo>.category').textContent = data.data.category
+                    document.querySelector('.creature>.basicInfo>.img').src = data.data.image
+                    document.querySelector('.creature>.basicInfo>.name').textContent = data.data.name
+                    document.querySelector('.creature>.location').textContent = data.data.common_locations.join(', ')
+                    document.querySelector('.creature>.details').textContent = data.data.description
+                    if (data.data.edible){
+                        document.querySelector('.creature>.cooked').textContent = `Cooking Effects: ${data.data.cooking_effect}`
+                        document.querySelector('.creature>.recovery').textContent = `${data.data.hearts_recovered} Hearts Recovered (Raw)`
+                    }
+                }
+                if (data.data.category === 'treasure'){
+                    document.querySelector('.treasure>.basicInfo>.category').textContent = data.data.category
+                    document.querySelector('.treasure>.basicInfo>.img').src = data.data.image
+                    document.querySelector('.treasure>.basicInfo>.name').textContent = data.data.name
+                    document.querySelector('.treasure>.location').textContent = data.data.common_locations.join(', ')
+                    document.querySelector('.treasure>.drops').textContent = data.data.drops.join(', ')
+                    document.querySelector('.treasure>.details').textContent = data.data.description
                 }
             })
             .catch(err => {
