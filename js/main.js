50b2223ab5f0653b51501aa1e54cf12c4a63284c
kyle <nojaim.k@gmail.com>
2024-08-08 21:45:40 -0400

    functionality for search restored

--- js/main.js
+++ js/main.js
@@ -1,16 +1,19 @@
 document.getElementById('entry').addEventListener('keypress', getEntry)
 
 
-// let url = `https://botw-compendium.herokuapp.com/api/v3/compendium/entry/${search}`
+
+// let urlWholeCompendium = 'https://botw-compendium.herokuapp.com/api/v3/compendium/all'
+
+// data.data.sort((a,b) => a.id - b.id)
 
 function getEntry(event){
     if(event.key === 'Enter'){
         event.preventDefault()
         let search = document.getElementById('entry').value
-        fetch(`https://botw-compendium.herokuapp.com/api/v3/compendium/all`)
+        let urlCompendiumSearch = `https://botw-compendium.herokuapp.com/api/v3/compendium/entry/${search}`
+        fetch(urlCompendiumSearch)
             .then(res => res.json())
             .then(data => {
-                data.data.sort((a,b) => a.id - b.id)
                 console.log(data)
             })
             .catch(err => {
